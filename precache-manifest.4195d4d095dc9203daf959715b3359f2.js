20282e95f4f73bf7dc00b03a7010a6e02a9d22e5
Ahmed Tan <ahmedtan@ahmeds-mbp.home>
2020-06-02 01:30:06 +0500

    Updates

---
+++ precache-manifest.4195d4d095dc9203daf959715b3359f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea88c3feb0fe02dc882acbb6d60c6eb",
+    "url": "/list-app/index.html"
+  },
+  {
+    "revision": "9ae17162bc7537ce0120",
+    "url": "/list-app/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "614402c02c68fd9b712b",
+    "url": "/list-app/static/js/2.bbf21a6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list-app/static/js/2.bbf21a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae17162bc7537ce0120",
+    "url": "/list-app/static/js/main.d831dcfa.chunk.js"
+  },
+  {
+    "revision": "6576be43f58d1e03a2ac",
+    "url": "/list-app/static/js/runtime-main.15708dcb.js"
+  }
+]);
